ded2d654e4e31bfe991115ed73ab5f60acdc1579
Aden1ke <sarahogunlalu@gmail.com>
2023-03-31 13:09:07 +0100

    updated 3_puts

--- 0x05-C-Pointers_arrays_strings/3-puts.c
+++ 0x05-C-Pointers_arrays_strings/3-puts.c
@@ -2,9 +2,9 @@
 void _puts(char *str)
 {
     int len = 0;
-   while (str[len] != '\0')
+    while (str[len] != '\0')
     {
-         _putchar(str[len]);
+        _putchar(str[len]);
         len++;
     }
     _putchar('\n');
