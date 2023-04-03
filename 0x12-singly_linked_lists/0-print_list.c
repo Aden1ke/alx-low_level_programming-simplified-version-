f6132de8508f579c5f174656d6a7bd14fb6ba4eb
Aden1ke <sarahogunlalu@gmail.com>
2023-04-03 21:41:43 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,11 @@ size_t print_list(const list_t *h)
     size_t count = 0;
     /*count is representing number of nodes,it should be initialized to zero to avoid printing gabbage value*/
 
-    while (h != NULL) 
+    while (h != NULL)
     /*as long as we haven't reached the end of the list*/
     {
         if (h->str != NULL)
-            /* 
+            /*
              * The step above checks if the str member of the current node (h) is NULL.
              * If it is not, it prints the string length of the string (len) and the string (str)
              *  followed by a newline character
@@ -28,7 +28,7 @@ size_t print_list(const list_t *h)
 
         h = h->next;
         count++;
-         /*
+        /*
          * The step moves h to point to the next node in the list (h->next)
          * and increments count by 1
          */
@@ -43,7 +43,7 @@ size_t print_list(const list_t *h)
 /*
  * NOTE:
  * size_t is a data type in C that is used to represent the size of an object in bytes.
- * It is commonly used to represent the sizes of arrays and other memory allocations, 
+ * It is commonly used to represent the sizes of arrays and other memory allocations,
  * as well as the lengths of data structures.
  * Therefore, using size_t as the return type for such functions ensures that the return value is both accurate
  * and portable across different platforms and compilers.
