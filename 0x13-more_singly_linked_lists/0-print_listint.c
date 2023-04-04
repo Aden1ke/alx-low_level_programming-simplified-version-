d6e78aaef22a492eec5cfeb1aafa99e1a5964306
Aden1ke <sarahogunlalu@gmail.com>
2023-04-04 11:33:49 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,8 +15,23 @@ size_t print_listint(const listint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
+        /*
+         * The step moves h to point to the next node in the list (h->next)
+         * and increments count by 1
+         */
 	}
 
 	return (count);
-
+/*
+     * the function returns the total number of nodes in the list, which is stored in count
+     */
 }
+
+/*
+ * NOTE:
+ * size_t is a data type in C that is used to represent the size of an object in bytes.
+ * It is commonly used to represent the sizes of arrays and other memory allocations,
+ * as well as the lengths of data structures.
+ * Therefore, using size_t as the return type for such functions ensures that the return value is both accurate
+ * and portable across different platforms and compilers.
+ */
