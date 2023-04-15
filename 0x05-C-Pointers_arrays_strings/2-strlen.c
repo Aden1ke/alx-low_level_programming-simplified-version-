879411ccf7451f43c555cf0229f743c23888638f
Aden1ke <sarahogunlalu@gmail.com>
2023-04-15 20:01:46 +0100

    source code

--- 0x05-C-Pointers_arrays_strings/2-strlen.c
+++ 0x05-C-Pointers_arrays_strings/2-strlen.c
@@ -18,3 +18,22 @@ int _strlen(char *s)
         ;
     return (len);
 }
+
+
+/**
+ * int len = 0; - This line declares and initializes an integer variable len to 0, 
+ * which will be used to store the length of the string. 
+ * This variable will be used to keep track of the number of characters in the input string.
+ * 
+ * while (s[len] != '\0') - This line starts a while loop that continues as long as the current character at the position len in the string s is not equal to the null terminator ('\0'), 
+ * which marks the end of the string. 
+ * The loop condition s[len] != '\0' checks if the character at the current position is not equal to '\0'.
+ * 
+ * len++ ; - This line increments the value of len by 1 for each iteration of the loop. 
+ * It effectively counts the number of characters in the string by moving to the next position in the string for each iteration of the loop.
+ * 
+ * return (len); - This line returns the final value of len, which represents the length of the string, 
+ * as the output of the function.
+ *  Once the loop completes and reaches the end of the string (i.e., encounters the null terminator), 
+ * the final value of len will be the total number of characters in the string.
+*/
